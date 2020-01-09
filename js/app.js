f0340051e5d28ee8596c40586edf36adc85a7c4e
Tashinga <mtokotashinga@gmail.com>
2020-01-10 00:08:19 +0200

    rought html/css components done

---
+++ js/app.js
@@ -0,0 +1,9 @@
+var templates = {
+    homepage: "1kDh6QNn25c3vOt5Q1Hu16NWc2wyq8j_CH364rxC67FA",
+    coverPage: "1Vu1T4IQSkcQyf-kFVBOwRElTp3r8XkXUr9MKfWypTJQ",
+    courseOutline: "1pKgDFm4Rubw07UXZmpzTeFFhWsmYNVb-ZjFnQSBxksg",
+    assignments: "1J-6c6CrvGM-WzT2mg46NxXYdnlkdOuhOxJ7eb3VVdkQ",
+    module: "12c8-QRDNi6XJ5XjnZnZw09RMLuy05U5E5-UUCJfH4XU",
+    page: "1ZUWwCMIlCwvtKEkfFulLVlC4CYmxUE-wMMTPhQGioyk",
+    quiz: "1WZiRccFuk58ZSH97pqDi5VPaOROy74tjuwiyywzSFo0",
+}
